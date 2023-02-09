41c60fc0ef87e6f0ff0409f98d560f4f164f7d4c
xgglt <1799583372@qq.com>
2023-02-10 00:00:42 +0800

    'dev home page .'

--- client/src/views/home/carousel.vue
+++ client/src/views/home/carousel.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="carousel-container">
-    <el-carousel height="600px">
+    <el-carousel height="64vh">
       <h3 class="title-seat" v-if="titleSeat">GUORAN FRUITS</h3>
       <h3 class="title animate__animated animate__heartBeat" ref="titleRef">
         GUORAN FRUITS
       </h3>
+      <button class="btn" @click="handleClick">进入在线商城</button>
       <el-carousel-item v-for="(item, index) in carousels" :key="index">
         <img :src="item" alt="" />
       </el-carousel-item>
@@ -13,26 +14,32 @@
 </template>
 
 <script setup>
-import { ref, onMounted, h } from "vue";
-import c1 from "@/assets/carousel-1.png";
-import c2 from "@/assets/carousel-2.png";
-import c3 from "@/assets/carousel-3.png";
-import c4 from "@/assets/carousel-4.png";
-import c5 from "@/assets/carousel-5.png";
+import { ref, onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+import c1 from '@/assets/carousel-1.png';
+import c2 from '@/assets/carousel-2.png';
+import c3 from '@/assets/carousel-3.png';
+import c4 from '@/assets/carousel-4.png';
+import c5 from '@/assets/carousel-5.png';
 
 const carousels = [c1, c2, c3, c4, c5];
+const router = useRouter();
+console.log(router);
 const titleRef = ref(null);
 let titleSeat = ref(false);
+
 onMounted(() => {
   const titleDiv = titleRef.value;
-  titleDiv.addEventListener("animationend", () => {
+  titleDiv.addEventListener('animationend', () => {
     titleSeat.value = true;
   });
 });
+
+const handleClick = () => router.push('/all');
 </script>
 
 <style lang="scss" scoped>
-@use "@/styles/var.scss" as *;
+@use '@/styles/var.scss' as *;
 :deep(.el-carousel) {
   position: relative;
 }
@@ -65,5 +72,24 @@ img {
   height: 100%;
   width: 100%;
   display: block;
+}
+.btn {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, calc(-50% + 94px));
+  z-index: 99;
+  border: none;
+  padding: 20px 60px;
+  background: #fff;
+  font-family: 微软雅黑;
+  color: rgb(0, 29, 93);
+  font-weight: bold;
+  cursor: pointer;
+  border: 1px solid #fff;
+  &:hover {
+    background: rgb(240, 240, 240);
+    border: 1px solid rgb(240, 240, 240);
+  }
 }
 </style>
--- client/src/views/home/index.vue
+++ client/src/views/home/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home-container">
-    <!-- <Carousel /> -->
+    <Carousel />
     <aboutUs />
   </div>
 </template>
