27080def33ca0a9bdd536105ce0ca8d609d35370
xgglt <1799583372@qq.com>
2023-03-23 18:02:02 +0800

    fix bug

--- backstage/src/views/product/type.vue
+++ backstage/src/views/product/type.vue
@@ -15,8 +15,8 @@
             <el-option label="1" :value="1" />
             <el-option label="2" :value="2" />
             <el-option label="3" :value="3" />
-            <el-option label="4" :value="3" />
-            <el-option label="5" :value="3" />
+            <el-option label="4" :value="4" />
+            <el-option label="5" :value="5" />
           </el-select>
         </template>
       </el-input>
